f1559da4deb3f2a476c00e341c3ed4b4d61edd79
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-07-02 09:12:03 -0400

    Cleaned up indentation.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+
   validates :answer_body, presence: true
   validates :question_id, presence: true
   validates_length_of :answer_body, :minimum => 50, :message => "Title must be more than 50 characters long."
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
-  validates :title, presence: true
-  validates :description, presence: true
   validates_length_of :title, :minimum => 40, :message => "Title must be more than 40 characters long."
   validates_length_of :description, :minimum => 150, :message => "Title must be more than 150 characters long."
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
 
